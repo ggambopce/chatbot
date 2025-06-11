52cf76b8fc84d6737ad3660adf4ebad9401301c6
ggambopce <ggambopce@gmail.com>
2025-06-11 20:58:52 +0900

    ✨ ERD 작성

--- chatbotBE/app.py
+++ chatbotBE/app.py
@@ -19,8 +19,8 @@ app = FastAPI()
 
 # CORS 설정
 origins = [
-    "http://127.0.0.1:5500",
-    "http://localhost:5500"
+    "http://127.0.0.1:5501",
+    "http://localhost:5501"
 ]
 
 app.add_middleware(
--- chatbotFE/main.js
+++ chatbotFE/main.js
@@ -1,7 +1,7 @@
 import { renderProfileForm } from "./components/ProfileForm.js";
 import { renderRandomProfile } from "./components/RandomProfileCard.js";
-import { renderSurvey } from "./components/SurveyForm.js";
 import { fetchSurveyQuestions } from './apis/survey.js';
+import { renderSurvey } from "./components/surveyForm.js";
 
 renderProfileForm(document.getElementById("formSection"));
 renderRandomProfile(document.getElementById("randomSection"));
